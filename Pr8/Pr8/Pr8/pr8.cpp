8d99288471e584aa16de821463a42d3b848cfd00
BillyKlebitz <44544376+BillyKlebitz@users.noreply.github.com>
2019-05-30 15:46:11 +0300

    Add files via upload

---
+++ Pr8/Pr8/Pr8/pr8.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	ifstream inp("inn.txt");
+	string input = "";
+	while (!inp.eof()) {
+		inp >> input;
+		cout << input << endl;
+	}
+	inp.close();
+	system("PAUSE");
+	return 0;
+	}
